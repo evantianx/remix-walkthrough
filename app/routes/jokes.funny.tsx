5a3b34bd2d1be0cc4726d14558d1b248b656889e
evantianx <evantianx@gmail.com>
2021-11-26 14:48:50 -0700

    routes: demo for nested child routes & unnested child routes
    Signed-off-by: evantianx <evantianx@gmail.com>

---
+++ app/routes/jokes.funny.tsx
@@ -0,0 +1,12 @@
+export default function JokesFunny() {
+  return (
+    <div>
+      <h1>Jokes funny</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam,
+        urna vitae aliquam aliquet, nisi urna tincidunt nunc, vitae accumsan
+        nunc urna vitae erat.
+      </p>
+    </div>
+  )
+}
